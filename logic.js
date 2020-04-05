19fe6c4b5de0507c49347be71b33e338cf3fc41f
pvellacott <vellacott.phil@gmail.com>
2020-04-04 23:05:31 -0700

    random content

--- logic.js
+++ logic.js
@@ -1,7 +1,22 @@
+const reply = [
+  "I dont understand what you said...",
+  "Can you try typing something that makes sense.. idiot.",
+  "Is that supposed to make sense?",
+  "Please try typing something that makes sense.",
+  "What you typed makes no sense.",
+  "Are you even trying?"
+]
+
+const randomReply = reply[Math.floor(Math.random() * reply.length)]
+console.log(randomReply)
+
 function textInput(text){
   if(event.key === 'Enter') {
-    let instruct = document.getElementById("user-input").value;
+    let instruct = document.getElementById("user-input");
     text.value = ''
+    
+    var list = "<li>" + randomReply + "</li>";
+    document.getElementById("content").innerHTML = list;
   }
 }
 
